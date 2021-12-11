d2381e41bd425329d4982d636fce5b365178a1e0
SAGAR_NIKUMBH <sagarnikumbh.weoto1991@gmail.com>
2021-12-11 18:37:23 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// create firework
+function Firework(sx, sy, tx, ty) {
+  // actual coordinates
+  this.x = sx;
+  this.y = sy;
+  // starting coordinates
+  this.sx = sx;
+  this.sy = sy;
+  // target coordinates
+  this.tx = tx;
+  this.ty = ty;
+  // distance from starting point to target
+  this.distanceToTarget = calculateDistance(sx, sy, tx, ty);
+  this.distanceTraveled = 0;
+}
+
+window.onload = function () {
+  var merrywrap = document.getElementById("merrywrap");
+  var box = merrywrap.getElementsByClassName("giftbox")[0];
+  var step = 1;
+  var stepMinutes = [2000, 2000, 1000, 1000];
+  function init() {
+    box.addEventListener("click", openBox, false);
+  }
+  function stepClass(step) {
+    merrywrap.className = "merrywrap";
+    merrywrap.className = "merrywrap step-" + step;
+  }
+  function openBox() {
+    if (step === 1) {
+      box.removeEventListener("click", openBox, false);
+    }
+    stepClass(step);
+    if (step === 3) {
+    }
+    if (step === 4) {
+      reveal();
+      return;
+    }
+    setTimeout(openBox, stepMinutes[step - 1]);
+    step++;
+  }
+
+  init();
+};
+
+function reveal() {
+  document.querySelector(".merrywrap").style.backgroundColor = "transparent";
+
+  loop();
+}
